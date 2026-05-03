9b838145036cd345d9552c36f4f0ae398715ffec
Navojit Podder <navojit.podder28@gmail.com>
2026-05-03 23:28:28 +0530

    Add text_cleaner

---
+++ AI-Text-Summarization-Module/Backend/utils/text_cleaner.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def clean_text(text):
+    return text.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
